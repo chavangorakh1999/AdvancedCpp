28d6622289c46d8d8c77ade7aa875f825b40f4b7
Gorakh Chavan <gorakhchavan@Gorakhs-MacBook-Pro.local>
2020-05-30 08:06:15 +0530

    Range Based For Loop

---
+++ RangeBasedFoorLoop/RangeBasedFoorLoop/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RangeBasedFoorLoop
+//
+//  Created by Gorakh Chavan on 30/05/20.
+//  Copyright © 2020 Gorakh Chavan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
