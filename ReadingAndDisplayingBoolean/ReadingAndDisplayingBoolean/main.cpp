6d63cfab4249fdf0e16edfc82b422fb37096b4f2
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-06-09 19:39:12 +0530

    using boolean as true or false

---
+++ ReadingAndDisplayingBoolean/ReadingAndDisplayingBoolean/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  ReadingAndDisplayingBoolean
+//
+//  Created by Gorakh Chavan on 09/06/20.
+//  Copyright © 2020 Gorakh Chavan. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool boolvalue;
+    cin>>boolalpha>>boolvalue;
+    cout<<boolalpha<<boolvalue<<endl;
+    return 0;
+}
+
