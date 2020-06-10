5d3a598f0f63f983eb90907c8f7f9db2b61a9aae
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-06-10 17:56:04 +0530

    using precision , width , fill

---
+++ PrecisionFillWidthParameter/PrecisionFillWidthParameter/main.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  PrecisionFillWidthParameter
+//
+//  Created by Gorakh Chavan on 10/06/20.
+//  Copyright © 2020 Gorakh Chavan. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout.precision(5);
+    cout<<124.5678<<endl;
+    cout<<"hi"<<endl;
+    
+    cout.width(10);
+    cout<<124.5678<<endl;
+    cout.width(10);
+    cout<<"hi"<<endl;
+    
+    cout.fill('*');
+    
+    cout.width(10);
+    cout<<124.5678<<endl;
+    cout.width(10);
+    cout<<"hi"<<endl;
+    
+    cout.setf(ios::left);
+    
+    cout.fill('*');
+    
+    cout.width(10);
+    cout<<124.5678<<endl;
+    cout.width(10);
+    cout<<"hi"<<endl;
+    
+    return 0;
+}
