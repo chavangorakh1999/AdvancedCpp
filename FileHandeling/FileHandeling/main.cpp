9d9a6b60867e09ae89fb7fb486b2e25f5bd33fd0
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-06-16 19:14:36 +0530

    seeking position with tellg ,tellp,seekp

--- FileHandeling/FileHandeling/main.cpp
+++ FileHandeling/FileHandeling/main.cpp
@@ -23,15 +23,35 @@ int main()
 //
 //        file.close();
 //    }
-    fstream file;
-    file.open("Readme.txt",ios :: in | ios :: out | ios :: app);
-    if(!file.is_open())
+//    fstream file;
+//    file.open("Readme.txt",ios :: in | ios :: out | ios :: app);
+//    if(!file.is_open())
+//        cout<<"Error finding file";
+//    else
+//    {
+//        cout<<file.tellg()<<endl;
+//        file<<"Just writing string to file"<<endl;
+//        file<<"can we do that ?"<<endl;
+//        cout<<file.tellg()<<endl;
+//        file.close();
+//    }
+    fstream write_file;
+    write_file.open("Readme.txt",ios :: in );
+    if(!write_file.is_open())
         cout<<"Error finding file";
     else
     {
-        file<<"Just writing string to file"<<endl;
-        file<<"can we do that ?"<<endl;
         
-        file.close();
+        cout<<write_file.tellg()<<endl;
+        cout<<write_file.tellp()<<endl;
+        
+//        write_file <<"We can grow together";
+        write_file.seekg(5);
+        cout<<write_file.tellg()<<endl;
+        cout<<write_file.tellp()<<endl;
+        
+        
+        write_file.close();
     }
+    
 }
