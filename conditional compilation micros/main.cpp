42a5b646b40f02fd15b6cdd5e835158cef30833d
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-06-29 15:36:24 +0530

    using conditional micros

--- conditional compilation micros/main.cpp
+++ conditional compilation micros/main.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 using namespace std;
+#define Mac 1
+#define Linux 2
+#define Win 3
+
+#define OS Mac
 
 int main()
 {
+    #if OS == Win
+    cout<<"Using windows"<<endl;
+    #endif
+
+    cout<<"Noraml print";
+
     return 0;
 }
