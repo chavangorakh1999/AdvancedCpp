d94838cddf9fe8281268594abbd6d08276a1d317
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-06-30 18:06:27 +0530

    explicit overloading

--- GenericFunction/main.cpp
+++ GenericFunction/main.cpp
@@ -3,7 +3,20 @@ using namespace std;
 
 template <typename genere> void display(genere x)
 {
+    cout<<"Inside generic function"<<endl;
     cout<<"You just passed "<< x <<endl;
+}
+
+void display(int x)
+{
+    cout<<"Inside normal function"<<endl;
+    cout<<"You just passed "<< x <<endl;
+}
+
+template <typename t1 ,typename t2> void Display(t1 x, t2 y)
+{
+    cout<<"First type "<< x <<endl;
+    cout<<"Second type "<< y <<endl;
 }
 
 int main()
@@ -11,6 +24,7 @@ int main()
     display(10);
     display(1.5);
     display("GRCSR");
+    Display(1.5,"GRC");
 
     return 0;
 }
