633c5b488a0d50b0d706fdbbcdfb9e2a033042c9
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-07-04 18:59:06 +0530

    passing multiple parameters to generic object.

--- GenericClass/class.cpp
+++ GenericClass/class.cpp
@@ -14,3 +14,18 @@ template <class T> class myClass
         }
 
 };
+template <class Type1= string ,class Type2 = int>class MyClass
+{
+    Type1 p1;
+    Type2 p2;
+    public:
+        MyClass(Type1 x,Type2 y)
+        {
+            p1=x;
+            p2=y;
+        }
+        void WhatYouGot()
+        {
+            cout<<"I got p1 = "<<p1<<" and"<<" p2 ="<<p2;
+        }
+};
--- GenericClass/main.cpp
+++ GenericClass/main.cpp
@@ -7,6 +7,9 @@ int main()
 {
     myClass<string> m("GRC","SR");
     m.whatYouGot();
-
+    MyClass <> ob1("GRC",15);
+    ob1.WhatYouGot();
+    MyClass <float> ob2(20.4,4);
+    ob2.WhatYouGot();
     return 0;
 }
