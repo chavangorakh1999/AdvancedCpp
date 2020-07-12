886532158a466032c4feec0f150ce9342c632e99
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-07-12 11:55:20 +0530

    kick start round D

---
+++ KickStart/RoundD2020/RoundD2020/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RoundD2020
+//
+//  Created by Gorakh Chavan on 12/07/20.
+//  Copyright © 2020 Gorakh Chavan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ KickStart/roundC/roundC/main.cpp
@@ -0,0 +1,50 @@
+//
+//  main.cpp
+//  roundC
+//
+//  Created by Gorakh Chavan on 11/07/20.
+//  Copyright © 2020 Gorakh Chavan. All rights reserved.
+//
+
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    long long int n;
+    int test_no,starter;
+    int arr[100000];
+    cin>>n;
+    
+    for(int i=0;i<n;i++)
+    {
+       cin>>test_no>>starter;
+    int ans_counter=0,decrease_counter=0;
+        for(int i=0;i<test_no;i++)
+        {
+            cin>>arr[i];
+        }
+        for(int i=0;i<test_no;i++)
+        {
+            if(arr[i]==arr[i-1]-1)
+            {
+                decrease_counter=decrease_counter+1;
+            }
+            else
+            {
+                decrease_counter=0;
+            }
+            if(arr[i]==1 and decrease_counter>=starter-1)
+            {
+                ans_counter=ans_counter+1;
+            }
+        }
+        cout<<"Case #1: "<<ans_counter<<endl;
+        
+        
+    }
+    
+    
+}
+
