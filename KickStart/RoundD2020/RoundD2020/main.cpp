c4bb7f81568a3d437b14d65aced8490057c27b4e
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-07-13 06:56:25 +0530

    prectising kick start day

--- KickStart/RoundD2020/RoundD2020/main.cpp
+++ KickStart/RoundD2020/RoundD2020/main.cpp
@@ -7,9 +7,46 @@
 //
 
 #include <iostream>
+using namespace std;
+int main()
+{
+    int arr[10000],n;
+    cin>>n;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    for(int j=0;j<n;j++)
+    {
+        int count=0;
+        
+        cin>>count;
+        for(int i=0;i<count;i++)
+        {
+            cin>>arr[i];
+        }
+        int breaking_day=0;
+        for(int i=0;i<count;i++)
+        {
+                
+                if(i==0 && arr[i+1]<arr[i])
+                {
+                    breaking_day=breaking_day+1;
+                }
+                else if(i>0)
+                {
+                    if(arr[i-1]<arr[i] && arr[i+1]<arr[i])
+                    {
+                    breaking_day=breaking_day+1;
+                    }
+                }
+                else if(i==count-1 && arr[i-1]<arr[i])
+                {
+                    breaking_day=breaking_day+1;
+                }
+                
+            
+        }
+        cout<<"Case #"<<j+1<<": "<<breaking_day<<endl;
+    }
+
     return 0;
 }
+
--- std:remove_const/main.cpp
+++ std:remove_const/main.cpp
@@ -1,24 +1,63 @@
-#include<iostream>
-#include<type_traits>
-using namespace std; 
+// #include<iostream>
+// #include<type_traits>
+// using namespace std; 
   
-int main() 
-{ 
+// int main() 
+// { 
 
-    typedef std::remove_const<int>::type first_variable; 
+//     typedef std::remove_const<int>::type first_variable; 
     
-    typedef std::remove_const<int* const>::type second_variable; 
+//     typedef std::remove_const<int* const>::type second_variable; 
     
-    cout << std::boolalpha; 
+//     cout << std::boolalpha; 
   
-    cout << "Does first_variable contain const int? "
-         << is_same<int, first_variable>::value 
-         << endl; 
+//     cout << "Does first_variable contain const int? "
+//          << is_same<int, first_variable>::value 
+//          << endl; 
   
    
   
-    cout << "Does second_variable contain const int? "
-         << is_same<int, second_variable>::value 
-         << endl; 
-    return 0; 
-} 
+//     cout << "Does second_variable contain const int? "
+//          << is_same<int, second_variable>::value 
+//          << endl; 
+//     return 0; 
+// } 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[10000],count,n;
+    cin>>n;
+    
+    for(int j=0;j<n+1;++j)
+    {
+        int breaking_day=0;
+        cin>>count;
+        for(int i=0;i<count;i++)
+        {
+            cin>>arr[i];
+        }
+        for(int i=0;i<count;i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+//        for(int i=0;i<count;i++)
+//        {
+//
+//                if((arr[i-1]<arr[i] && i>0 )|| arr[i]==count+1)
+//                {
+//                breaking_day=breaking_day+1;
+//                }
+//                else if(i==0 || arr[i+1]<arr[i])
+//                {
+//                    breaking_day=breaking_day+1;
+//                }
+//        }
+//        cout<<"Case #"<<i+1<<": "<<breaking_day<<endl;
+//
+        
+    }
+    return 0;
+}
